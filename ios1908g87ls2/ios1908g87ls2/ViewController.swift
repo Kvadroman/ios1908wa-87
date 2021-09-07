4cf1fc098ecdb12fa470cd09d7892b901423a1bd
Kvadroman <kvadrostoplerman@gmail.com>
2021-09-07 17:42:05 +0300

    ls3 Assignment #2 Block2
    Задача 2.

--- ios1908g87ls2/ios1908g87ls2/ViewController.swift
+++ ios1908g87ls2/ios1908g87ls2/ViewController.swift
@@ -11,13 +11,24 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentProfit(money: 24, years: 195, percent: 1.06)
+       currentProfit(money: 24, years: 195, percent: 1.06)
+       summaDeneg(grants: 700)
     }
     
     func currentProfit (money: Double, years: Double, percent: Double ) {
         print(money*years*percent)
     }
     
-    
+    func summaDeneg (grants: Double) {
+        var expenses = 1000.00
+        let y = 0.03
+        var summaTrat = 1000.00
+        for _ in 1...10 {
+            expenses = expenses * y + expenses
+            summaTrat = summaTrat + expenses
+            
+        }
+        print(summaTrat - grants*10)
+    }
 }
-
+    
