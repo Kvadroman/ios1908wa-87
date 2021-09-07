fb8296f22382d904db906730cc8078cd6a1eb3a7
Kvadroman <kvadrostoplerman@gmail.com>
2021-09-07 18:34:59 +0300

    ls 3 Assignment #2. Block 2
    Задача 3

--- ios1908g87ls2/ios1908g87ls2/ViewController.swift
+++ ios1908g87ls2/ios1908g87ls2/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
        currentProfit(money: 24, years: 195, percent: 1.06)
        summaDeneg(grants: 700)
+       summaMonth(grants: 700)
     }
     
     func currentProfit (money: Double, years: Double, percent: Double ) {
@@ -21,14 +22,30 @@ class ViewController: UIViewController {
     
     func summaDeneg (grants: Double) {
         var expenses = 1000.00
-        let y = 0.03
+        let percent = 0.03
         var summaTrat = 1000.00
         for _ in 1...10 {
-            expenses = expenses * y + expenses
+            expenses = expenses * percent + expenses
             summaTrat = summaTrat + expenses
-            
         }
         print(summaTrat - grants*10)
+    }
+    
+    func summaMonth (grants: Double) {
+        let nakopleniya = 2400.00
+        let percent = 0.03
+        var expenses = 1000.00
+        var summaTrat = 1000.00
+        while ((nakopleniya + grants*10) - summaTrat) >= 0 {
+            for i in 1...10 {
+                expenses = expenses * percent + expenses
+                summaTrat = summaTrat + expenses
+                if summaTrat >= (nakopleniya + grants*10) {
+                    break
+                }
+                print(i)
+            }
+        }
     }
 }
     
